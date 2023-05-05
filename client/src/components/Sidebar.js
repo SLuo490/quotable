d8c5facbf3dfe8b8b68479d09991c932d7c52ece
SLuo5991 <sluo5991@gmail.com>
2023-05-04 21:23:47 -0400

    add sidebar with options

---
+++ client/src/components/Sidebar.js
@@ -0,0 +1,91 @@
+import { useState, useEffect } from 'react';
+import '../styles/sidebar.css';
+
+export default function Sidebar() {
+  const [categories, setCategories] = useState([]);
+
+  useEffect(() => {
+    setCategories([
+      'Age',
+      'Alone',
+      'Amazing',
+      'Anger',
+      'Architecture',
+      'Art',
+      'Attitude',
+      'Beauty',
+      'Best',
+      'Birthday',
+      'Business',
+      'Car',
+      'Change',
+      'Communications',
+      'Computers',
+      'Cool',
+      'Courage',
+      'Dad',
+      'Dating',
+      'Death',
+      'Design',
+      'Dreams',
+      'Environmental',
+      'Education',
+      'Equality',
+      'Experience',
+      'Failure',
+      'Faith',
+      'Family',
+      'Famous',
+      'Fear',
+      'Fitness',
+      'Food',
+      'Forgiveness',
+      'Freedom',
+      'Friendship',
+      'Funny',
+      'Future',
+      'God',
+      'Good',
+      'Government',
+      'Graduation',
+      'Great',
+      'Happiness',
+      'Health',
+      'History',
+      'Home',
+      'Hope',
+      'Humor',
+      'Imagination',
+      'Inspirational',
+      'Intelligence',
+      'Jealousy',
+      'Knowledge',
+      'Leadership',
+      'Learning',
+      'Legal',
+      'Life',
+      'Love',
+      'Marriage',
+      'Medical',
+      'Men',
+      'Mom',
+      'Money',
+      'Morning',
+      'Movies',
+      'Success',
+    ]);
+  }, []);
+
+  return (
+    <div
+      className='list-unstyled flex-grow-1 overflow-auto'
+      style={{ overflowY: 'scroll', height: 'calc(100vh - 70px)' }}
+    >
+      {categories.map((category, index) => (
+        <div className='text-center pt-2' key={index}>
+          <button className='btn w-50'> {category}</button>
+        </div>
+      ))}
+    </div>
+  );
+}
--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,10 +1,18 @@
 import Nav from '../components/Nav';
+import Sidebar from '../components/Sidebar';
 
 export default function Home() {
   return (
     <div>
       <Nav />
-      <h1>Home Page</h1>
+      <div className='row'>
+        <div className='col-sm'>
+          <Sidebar />
+        </div>
+        <div className='col-sm'>
+          <h1>Content</h1>
+        </div>
+      </div>
     </div>
   );
 }
